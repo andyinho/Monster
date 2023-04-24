0fa294216c2182437a49cf29179028e47228d7bc
Andres <freetrialandy@gmail.com>
2023-04-24 00:14:58 -0700

    add monster and player attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,42 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 12;
+const STRONG_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
-let currentMosterHealth = chosenMaxLife;
+let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
+function attackMonster(mode) {
+  let maxDamage;
+
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const monsterDamage = dealMonsterDamage(maxDamage);
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentMonsterHealth -= monsterDamage;
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("It's a tie!");
+  }
+}
+
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMosterHealth -= damage;
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
