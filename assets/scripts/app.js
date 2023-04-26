36603ee81a2e09fa41b5bbb4b39c7ecf598ef3ff
Andres <freetrialandy@gmail.com>
2023-04-25 19:44:40 -0700

    add heal ability for player

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,13 @@
 const ATTACK_VALUE = 10;
-const MONSTER_ATTACK_VALUE = 12;
-const STRONG_ATTACK_VALUE = 14;
+const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
+const HEAL_INCREASE = 3;
+const HEAL_COUNT = 0;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let healBtnCount = HEAL_COUNT;
 
 adjustHealthBars(chosenMaxLife);
 
@@ -38,5 +41,17 @@ function strongAttackHandler() {
   attackMonster('STRONG_ATTACK');
 }
 
+function healPLayerHandler() {
+  if (healBtnCount <= 0) {
+    increasePlayerHealth(HEAL_INCREASE);
+    console.log(healBtnCount);
+  } else if (healBtnCount >= 1) {
+    alert('You have used all your heal abilities!');
+    console.log(healBtnCount);
+  }
+  healBtnCount++;
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPLayerHandler);
