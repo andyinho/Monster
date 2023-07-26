f09524393f7b258fa75db3740fd34ed270d0678c
Andres <freetrialandy@gmail.com>
2023-07-25 22:08:52 -0700

    commit test push

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -209,3 +209,5 @@ attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
 healBtn.addEventListener('click', healPLayerHandler);
 logBtn.addEventListener('click', printLogHandler);
+
+// test push!
